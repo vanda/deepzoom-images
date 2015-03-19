7a04fccb17bb78ac35bd4911e0c59169a75b035e
James Doc <jamesdoc@gmail.com>
2015-03-19 16:06:59 +0000

    Document generating tiles

--- readme.md
+++ readme.md
@@ -1,3 +1,9 @@
 # ImageZoomModule
 
 
+## Generate tiles
+Dev set up is using [this image](http://www-images.theonering.org/torwp/wp-content/uploads/2013/11/HDOS_TAPESTRY_Domestic.jpg) until the final scroll is completed by Liam.
+
+1. Download image
+2. Modify `tile_generator/generate.py:7&14` to point to the correct file
+3. Run `python tile_generator/generate.py`
---
+++ tile_generator/generate.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import deepzoom
+
+# Specify your source image
+SOURCE = "source.jpg"
+
+# Create Deep Zoom Image creator with weird parameters
+creator = deepzoom.ImageCreator(tile_size=128, tile_overlap=2, tile_format="png",
+                                image_quality=0.8, resize_filter="bicubic")
+
+# Create Deep Zoom image pyramid from source
+creator.create(SOURCE, "../www/assets/tiles/source.dzi")
